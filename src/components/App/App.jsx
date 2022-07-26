f8acf9e4f1a711f7d8e5b4479697146d018779f8
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-07-26 16:23:20 +0300

    useLocalStorage added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
+import css from './App.module.css';
+
 import ContactList from 'components/ContactList/ContactList';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import { Toaster } from 'react-hot-toast';
 
-import css from './App.module.css';
-import { useState, useEffect } from 'react';
-
 const LC_KEY = 'phonebook';
 
+// ------ Option 1: with custom hook useLocalStorage ------
+
 const App = () => {
-  const [error, setError] = useState(null);
-  const [contacts, setContacts] = useState(checkLocalStorage);
+  const [contacts, setContacts] = useLocalStorage(LC_KEY, []);
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem(LC_KEY, JSON.stringify(contacts));
-
-    if (contacts.length !== 0 && error) {
-      setError(null);
-    }
-  }, [contacts, error]);
-
-  function checkLocalStorage() {
-    try {
-      const dataFromLocStorage = JSON.parse(localStorage.getItem(LC_KEY));
-      return dataFromLocStorage ? dataFromLocStorage : [];
-    } catch (err) {
-      setError('LocalStorage is corrupted :(');
-      return [];
-    }
-  }
 
   function updateContacts(contact) {
     setContacts([...contacts, contact]);
@@ -55,7 +39,7 @@ const App = () => {
             onDeleteContact={deleteContact}
           />
         )}
-        {error && <p>{error}</p>}
+        {/* {error && <p>{error}</p>} */}
       </div>
       <Toaster position="top-left" />
     </>
@@ -63,3 +47,60 @@ const App = () => {
 };
 
 export default App;
+
+// ------ Option 2: without custom hooks ------
+
+// const App = () => {
+//   const [error, setError] = useState(null);
+//   const [contacts, setContacts] = useState(checkLocalStorage);
+//   const [filter, setFilter] = useState('');
+
+//   useEffect(() => {
+//     localStorage.setItem(LC_KEY, JSON.stringify(contacts));
+
+//     if (contacts.length !== 0 && error) {
+//       setError(null);
+//     }
+//   }, [contacts, error]);
+
+//   function checkLocalStorage() {
+//     try {
+//       const dataFromLocStorage = JSON.parse(localStorage.getItem(LC_KEY));
+//       return dataFromLocStorage ? dataFromLocStorage : [];
+//     } catch (err) {
+//       setError('LocalStorage is corrupted :(');
+//       return [];
+//     }
+//   }
+
+//   function updateContacts(contact) {
+//     setContacts([...contacts, contact]);
+//   }
+
+//   function deleteContact(id) {
+//     setContacts(prevState => prevState.filter(contact => contact.id !== id));
+//   }
+
+//   return (
+//     <>
+//       <div className={css.container}>
+//         <h1 className={css.title__phonebook}>Phonebook</h1>
+//         <ContactForm contacts={contacts} onUpdateContacts={updateContacts} />
+
+//         <h2 className={css.title__contacts}>Contacts</h2>
+//         <Filter filter={filter} onFilterChange={setFilter} />
+//         {contacts.length !== 0 && (
+//           <ContactList
+//             contacts={contacts}
+//             filter={filter}
+//             onDeleteContact={deleteContact}
+//           />
+//         )}
+//         {error && <p>{error}</p>}
+//       </div>
+//       <Toaster position="top-left" />
+//     </>
+//   );
+// };
+
+// export default App;
